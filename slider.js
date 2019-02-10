e6ec7eeb33855d799f781379c65ed4b895603b8e
Adamkopan <47123371+Adamkopan@users.noreply.github.com>
2019-02-10 18:42:21 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -2,6 +2,9 @@ const h1Tittle = document.querySelector(".tittleOfSlide");
 const divSlide = document.querySelector(".slide img");
 const span = document.querySelector("span");
 
+let singleSlideTime = 10000;
+let singleLoopTime = 50000;
+
 const slidesImage = [
   "CSS3logopng400.png",
   "html5logo400.png",
@@ -22,16 +25,16 @@ const slider = () => {
     setTimeout(() => {
       h1Tittle.textContent = tittleSlides[index];
       span.textContent = `${index + 1}/6`;
-    }, 10000 * index);
+    }, singleSlideTime * index);
   });
 
   slidesImage.forEach((item, index) => {
     setTimeout(() => {
       divSlide.src = `img/${slidesImage[index]}`;
-    }, 10000 * index);
+    }, singleSlideTime * index);
   });
 
-  setTimeout(slider, 50000);
+  setTimeout(slider, singleLoopTime);
 };
 
 slider();
